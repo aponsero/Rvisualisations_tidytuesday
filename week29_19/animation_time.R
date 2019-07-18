902b94b46d2795c0fd808cac47bfd6dd92749162
Alise Ponsero <aponsero@email.arizona.edu>
2019-07-18 09:29:29 -0700

    week 29

--- week29_19/animation_time.R
+++ week29_19/animation_time.R
@@ -3,41 +3,43 @@ library(dplyr)
 library(gganimate)
 library(gapminder)
 library(tidyverse)
+library(lubridate)
+library(extrafont)
+font_import()
+loadfonts()
 
 
-#####################tutorial from gganimate###################
-ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, colour = country)) +
-  geom_point(alpha = 0.7, show.legend = FALSE) +
-  scale_colour_manual(values = country_colors) +
-  scale_size(range = c(2, 12)) +
-  scale_x_log10() +
-  facet_wrap(~continent) +
-  # Here comes the gganimate specific bits
-  labs(title = 'Year: {frame_time}', x = 'GDP per capita', y = 'life expectancy') +
-  transition_time(year) +
-  ease_aes('linear')
-
-
-########################### Tidydata ###########################
+#Load dataset
 r4ds_members <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-16/r4ds_members.csv")
 r4ds_members
 
-data_plot <- r4ds_members %>% select(date, total_membership, 
-                                     daily_active_members,
-                                     weekly_active_members) %>% gather(members, count, -date)
+data_plot <- r4ds_members %>% 
+                    select(date, total_membership,daily_active_members,weekly_active_members) %>% 
+                    gather(members, count, -date) 
+
 
 
 # static plot time-serie
 data_plot %>% ggplot(aes(x = date, y = count)) + 
+  labs(y="number of members", x="date",
+       title="Number of members in R4DS\n(2017 to 2019)")+
   geom_line(aes(color = members), size = 1) +
   scale_color_manual(values = c("#00AFBB", "#E7B800", "red")) +
-  theme_minimal()
+  geom_point(aes(color = members), size = 3) +
+  theme_minimal(base_size = 16)+
+  theme(text=element_text(family="Times New Roman"))
 
-# animation galore
+# animation ! exciting times !
 data_plot %>% ggplot(aes(x = date, y = count)) + 
+  labs(y="number of members", x="date",
+       title="Number of members in R4DS",
+       subtitle = "(2017 to 2019)")+
   geom_line(aes(color = members), size = 1) +
   scale_color_manual(values = c("#00AFBB", "#E7B800", "red")) +
-  theme_minimal()+
-# Here comes the gganimate specific bits
-  geom_point() +
-  transition_reveal(date)
+  geom_point(aes(color = members), size = 3) +
+  theme_minimal(base_size = 16)+
+  theme(text=element_text(family="Times New Roman"))+
+# Here comes the gganimate stuff
+  transition_reveal(date,
+                    range = as.Date(c("2017-08-27", "2020-01-01")))
+
