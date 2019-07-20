599832384c517ee22de5eb11c3f8db0898ef90be
Alise Ponsero <aponsero@email.arizona.edu>
2019-07-20 11:25:20 -0700

    week 29

--- week29_19/animation_time.R
+++ week29_19/animation_time.R
@@ -11,8 +11,6 @@ loadfonts()
 
 #Load dataset
 r4ds_members <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-16/r4ds_members.csv")
-r4ds_members
-
 data_plot <- r4ds_members %>% 
                     select(date, total_membership,daily_active_members,weekly_active_members) %>% 
                     gather(members, count, -date) 
@@ -24,22 +22,35 @@ data_plot %>% ggplot(aes(x = date, y = count)) +
   labs(y="number of members", x="date",
        title="Number of members in R4DS\n(2017 to 2019)")+
   geom_line(aes(color = members), size = 1) +
-  scale_color_manual(values = c("#00AFBB", "#E7B800", "red")) +
+  scale_color_manual(values = c("brown1","darkolivegreen4","burlywood3"),
+                     labels=c("daily active members", "total members",
+                              "weekly active members")) +
   geom_point(aes(color = members), size = 3) +
   theme_minimal(base_size = 16)+
-  theme(text=element_text(family="Times New Roman"))
+  theme(text=element_text(family="Times New Roman"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"))
 
 # animation ! exciting times !
-data_plot %>% ggplot(aes(x = date, y = count)) + 
+my_anim <- data_plot %>% ggplot(aes(x = date, y = count)) + 
   labs(y="number of members", x="date",
-       title="Number of members in R4DS",
-       subtitle = "(2017 to 2019)")+
+       title="Number of members in R4DS\n(2017 to 2019)")+
   geom_line(aes(color = members), size = 1) +
-  scale_color_manual(values = c("#00AFBB", "#E7B800", "red")) +
+  scale_color_manual(values = c("brown1","darkolivegreen4","burlywood3"),
+                     labels=c("daily active members", "total members",
+                              "weekly active members")) +
   geom_point(aes(color = members), size = 3) +
   theme_minimal(base_size = 16)+
-  theme(text=element_text(family="Times New Roman"))+
+  theme(text=element_text(family="Times New Roman"),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(colour = "black"))+
 # Here comes the gganimate stuff
   transition_reveal(date,
                     range = as.Date(c("2017-08-27", "2020-01-01")))
 
+animate(my_anim, height = 350, width =650)
+
+anim_save("time_series1.gif", animation = last_animation(), 
+          path = "/Users/aponsero/Documents/Rvisualisations_tidytuesday/week29_19")
